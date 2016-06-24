14b9d86572ee7ac7a066eb380ac44ff53501a0c4
chenming <chenming@wasu.com>
2016-06-24 09:56:13 +0800

    Initial commit add a class

---
+++ app/src/main/java/com/example/administrator/learngit/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.administrator.learngit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+
+/**
+ * Created by chenming on 9:55 2016/6/24.
+ */
+
+public class SecondActivity extends AppCompatActivity implements DialogInterface.OnClickListener {
+
+
+	@Override
+	public void onClick(DialogInterface dialogInterface, int i) {
+
+	}
+}
